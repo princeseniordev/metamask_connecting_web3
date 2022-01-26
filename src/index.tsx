8fb5fd396c75d0356e7557be6cb1ba0667b569d2
princeseniordev <princeseniordev@gmail.com>
2022-01-26 22:41:53 +0300

    add chakra ui and dappprovider

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
+// Import DappProvider
+import { DAppProvider } from '@usedapp/core';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* {
+      Wrap our app in the provider, config is required, 
+        but can be left as an empty object:
+    } */}
+    <DAppProvider config={{}}>
       <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
