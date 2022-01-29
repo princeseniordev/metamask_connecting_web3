a89185c5a296aa3fd0aae7ffcc49dfe9e15baf5d
princeseniordev <princeseniordev@gmail.com>
2022-01-29 12:58:09 +0300

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from './components/Layout';
 import ConnectButton from './components/ConnectButton';
+import AccuontModal from './components/AccountModal';
 
 export default function App() {
+  // Pull the disclosure methods
   return (
     // lets us use Chakra UI syntax across our app:
     <ChakraProvider>
       <Layout>
+        {/* Our connect button will only handle opening */}
         <ConnectButton />
+        <AccuontModal />
       </Layout>
     </ChakraProvider>
   )
---
+++ src/components/AccountModal.tsx
@@ -0,0 +1,147 @@
+// AccountModal.tsx
+import { 
+  Box, 
+  Button, 
+  Flex, 
+  Modal, 
+  ModalBody, 
+  ModalCloseButton, 
+  ModalContent, 
+  ModalHeader, 
+  ModalOverlay, 
+  Text,
+  Link,
+  ModalFooter,
+} from '@chakra-ui/react';
+import { ExternalLinkIcon, CopyIcon } from '@chakra-ui/icons';
+import { useEthers } from '@usedapp/core';
+import Identicon from './Identicon';
+
+export default function AccountModal() {
+  const { account, deactivate } = useEthers();
+  
+  return (
+    <Modal>
+      <ModalOverlay />
+      <ModalContent
+        background="gray.900"
+        border="1px"
+        borderStyle="solid"
+        borderColor="gray.700"
+        borderRadius="3xl"
+      >
+        <ModalHeader color="white" px="4" fontSize="lg" fontWeight="medium">
+          Account
+        </ModalHeader>
+        <ModalCloseButton 
+          color="white"
+          fontSize="sm"
+          _hover={{
+            color: "whiteAlpha.700",
+          }}
+        />
+        <ModalBody pt={0} px={4}>
+          <Box
+            borderRadius="3xl"
+            border="1px"
+            borderStyle="solid"
+            borderColor="gray.600"
+            px={5}
+            pt={4}
+            pb={2}
+            mb={3}
+          >
+            <Flex justifyContent="space-between" alignItems="center" mb={3}>
+              <Text color="gray.400" fontSize="sm">
+                Connected with MetaMask
+              </Text>
+              <Button
+                variant="outline"
+                size="sm"
+                borderColor="blue.800"
+                borderRadius="3xl"
+                color="blue.500"
+                fontSize="13px"
+                fontWeight="normal"
+                px={2}
+                height="26px"
+                _hover={{
+                  background: "none",
+                  borderColor: "blue.300",
+                  textDecoration: "underline",
+                }}
+              >
+                Change
+              </Button>
+            </Flex>
+            <Flex alignItems="center" mt={2} mb={4} lineHeight={1}>
+              <Identicon />
+              <Text
+                color="white"
+                fontSize="xl"
+                fontWeight="semibold"
+                ml="2"
+                lineHeight="1.1"
+              >
+                {account &&
+                  `${account.slice(0, 6)}...${account.slice(
+                    account.length - 4,
+                    account.length
+                  )}`
+                }
+              </Text>
+            </Flex>
+            <Flex alignContent="center" m={3}>
+              <Button
+                variant="link"
+                color="gray.400"
+                fontWeight="normal"
+                fontSize="sm"
+                _hover={{
+                  textDecoration: "none",
+                  color: "whiteAlpha.800",
+                }}
+              >
+                <CopyIcon mr={1} />
+                  Copy Address
+              </Button>
+              <Link
+                fontSize="sm"
+                display="flex"
+                alignItems="center"
+                href={`https://ropsten.etherscan.io/address/${account}`}
+                isExternal
+                color="gray.400"
+                ml={6}
+                _hover={{
+                  color: "whiteAlpha.800",
+                  textDecoration: 'underline',
+                }}
+              >
+                <ExternalLinkIcon mr={1} />
+                View on Explorer
+              </Link>
+            </Flex>
+          </Box>
+        </ModalBody>
+
+        <ModalFooter
+          justifyContent="end"
+          background="gray.700"
+          borderBottomLeftRadius="3xl"
+          borderBottomRightRadius="3xl"
+          p={6}
+        >
+          <Text
+            color="white"
+            textAlign="left"
+            fontWeight="medium"
+            fontSize="md"
+          >
+            Your transactions will appear
+          </Text>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
